37e0cc6e4d31a5efcaa3adb81d44b82504e374a3
rami <rami.albacha@yahoo.com>
2020-01-09 18:19:47 +0100

    added an example for bigleaf sunlit-shaded canopy

---
+++ examples/bigleaf_sunlit_shaded_at_one_hour/sim.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from matplotlib import pyplot
+
+from crop_energy_balance.crop import Canopy
+from crop_energy_balance.inputs import SunlitShadedInputs
+from crop_energy_balance.params import Params
+from crop_energy_balance.solver import Solver
+from crop_energy_balance.utils import convert_kelvin_to_celsius
+
+
+def plot_temperature_profile(canopy_object: Canopy, fig_path: Path):
+    soil_component_key = -1
+    leaf_layer_key = 0
+    soil_component_temperature = convert_kelvin_to_celsius(canopy_object[-1].temperature)
+    sunlit_component_temperature = convert_kelvin_to_celsius(canopy_object[leaf_layer_key]['sunlit'].temperature)
+    shaded_component_temperature = convert_kelvin_to_celsius(canopy_object[leaf_layer_key]['shaded'].temperature)
+
+    fig, ax = pyplot.subplots()
+    ax.plot(soil_component_temperature, soil_component_key, marker='o', markerfacecolor='brown')
+    ax.plot(sunlit_component_temperature, leaf_layer_key, marker='o', markerfacecolor='yellow')
+    ax.plot(shaded_component_temperature, leaf_layer_key, marker='o', markerfacecolor='orange')
+    ax.axvline(convert_kelvin_to_celsius(canopy_object.inputs.air_temperature), label='air')
+    ax.set(xlabel='temperature [°C]', ylabel='Component index [-]', xlim=(20, 30))
+    ax.grid(True)
+    fig.savefig(fig_path)
+    pyplot.close()
+
+
+if __name__ == '__main__':
+    root_pth = Path(__file__).parent
+
+    inputs = SunlitShadedInputs(root_pth / 'inputs_well_watered.json')
+    params = Params(root_pth / 'params.json')
+    params.update(inputs=inputs)
+
+    canopy = Canopy(leaves_category='sunlit-shaded',
+                    inputs=inputs,
+                    params=params)
+    Solver(canopy=canopy,
+           inputs=inputs,
+           params=params).run()
+
+    plot_temperature_profile(canopy, root_pth / 'temperature_profile.png')
