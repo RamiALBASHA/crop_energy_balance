feb30044cf674f59e5200ceb0849fd29f39e7a70
ralbasha <rami.albasha@itk.fr>
2020-01-28 19:18:08 +0100

    wip for icropm2020

--- examples/icropm2020/one_day_comparison/sim.py
+++ examples/icropm2020/one_day_comparison/sim.py
@@ -1,19 +1,6 @@
-from ephem import Sun, Observer
-from datetime import datetime
+from math import radians
 
-
-latitude = 43.16
-longitude = 3.87
-
-
-def get_solar_inclination(date: datetime) -> float:
-    sun = Sun()
-    obs = Observer()
-    obs.lat = latitude
-    obs.lon = longitude
-    obs.date = date
-    sun.compute(obs)
-    return sun.
+from crop_irradiance.uniform_crops import inputs, params, shoot
 
 hourly_direct_par = [0, 0, 0, 0, 0, 0, 0, 18.428820397314112, 86.779379638514726, 213.23527694182002,
                      332.53550250736947, 379.40012173450828, 359.77089500962774, 277.64770928541054,
@@ -22,9 +9,37 @@ hourly_diffuse_par = [0, 0, 0, 0, 0, 0, 0, 18.428820397314112, 61.4976272200411,
                       74.699625542626109,
                       85.077684125431659, 80.552408749094454, 65.365757925282779, 72.846107552867508,
                       37.432721347065836, 1.1288678878776721, 0, 0, 0, 0, 0, 0, 0]
-hourly_solar_angle = [-15.86]
+hourly_solar_angle = [-22.15, - 22.91, -20.89, -16.32, -9.67, -0.67, 8.2, 18.34, 28.98, 39.81, 50.46, 60.27,
+                      67.68, 69.63, 64.79, 56.01, 45.7, 34.91, 24.13, 13.67, 3.93, -5.28, -12.9, -18.68]
 
-sun = Sun()
-sun.compute(datetime(2012,7,20, 12))
+leaf_layers = {3: 1.0,
+               2: 1.0,
+               1: 1.0,
+               0: 1.0}
 
 
+def get_simulated_irradiance(leaves_category: str, hour: int):
+    if leaves_category == 'sunlit-shaded':
+        sim_inputs = inputs.SunlitShadedInputs(leaf_layers=leaf_layers,
+                                               incident_direct_irradiance=hourly_direct_par[hour],
+                                               incident_diffuse_irradiance=hourly_diffuse_par[hour],
+                                               solar_inclination=radians(hourly_solar_angle[hour]))
+
+        sim_params = params.SunlitShadedParams(leaf_reflectance=0.07, leaf_transmittance=0.00,
+                                               leaves_to_sun_average_projection=0.5, sky_sectors_number=3,
+                                               sky_type='soc',
+                                               canopy_reflectance_to_diffuse_irradiance=0.057)
+        sim_params.update(sim_inputs)
+    else:
+        sim_inputs = inputs.LumpedInputs(leaf_layers=leaf_layers,
+                                         incident_irradiance=hourly_direct_par[hour] + hourly_diffuse_par[hour])
+        sim_params = params.LumpedParams(extinction_coefficient=0.5)
+
+    canopy = shoot.Shoot(leaves_category=leaves_category, inputs=sim_inputs, params=sim_params)
+
+    absorbed_irradiance = {index: layer.absorbed_irradiance for index, layer in canopy.items()}
+    soil_irradiance = (hourly_direct_par[hour] + hourly_diffuse_par[hour]) - (
+        sum([sum(v.absorbed_irradiance.values()) for v in canopy.values()]))
+    absorbed_irradiance.update({-1: soil_irradiance})
+
+    return absorbed_irradiance
