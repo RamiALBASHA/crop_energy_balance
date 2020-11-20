dd9ab21920aff25f9190ba8291d33d51f8f3b17b
ralbasha <rami.albasha@itk.fr>
2020-11-20 18:20:08 +0100

    debugged to cope with the crop_irradiance v.1.0.0

--- examples/icropm2020/one_day_comparison/sim.py
+++ examples/icropm2020/one_day_comparison/sim.py
@@ -1,13 +1,14 @@
 from json import load
 from math import radians
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
 import pandas as pd
 from crop_irradiance.uniform_crops import (
     inputs as irradiance_inputs, params as irradiance_params, shoot as irradiance_canopy)
 
 from crop_energy_balance import (
-    inputs as eb_inputs, params as eb_params, crop as eb_canopy, solver as eb_solver, utils)
+    inputs as eb_inputs, params as eb_params, crop as eb_canopy, solver as eb_solver)
+from crop_energy_balance.examples.sources.demo import get_weather_data
 
 with open('inputs.json', mode='r') as f:
     json_inputs = load(f)
@@ -22,53 +23,51 @@ leaf_layers = {3: 1.0,
 
 
 def get_irradiance_sim_inputs_and_params(
-        leaf_scale_type: str,
-        leaf_class_type: str,
+        is_bigleaf: bool,
+        is_lumped: bool,
         incident_direct_par_irradiance: float,
         incident_diffuse_par_irradiance: float,
         solar_inclination_angle: float) -> (
         irradiance_inputs.LumpedInputs or irradiance_inputs.SunlitShadedInputs,
         irradiance_params.LumpedParams or irradiance_params.SunlitShadedInputs):
-    if leaf_scale_type == 'bigleaf':
-        vegetative_layers = {0: sum(leaf_layers.values())}
-    else:
-        vegetative_layers = leaf_layers.copy()
+    vegetative_layers = {0: sum(leaf_layers.values())} if is_bigleaf else leaf_layers.copy()
 
-    if leaf_class_type == 'lumped':
-        sim_inputs = irradiance_inputs.LumpedInputs(
-            leaf_layers=vegetative_layers,
-            incident_irradiance=incident_direct_par_irradiance + incident_diffuse_par_irradiance)
-        sim_params = irradiance_params.LumpedParams(
-            extinction_coefficient=0.45)
+    common_inputs = dict(
+        leaf_layers=vegetative_layers,
+        incident_direct_irradiance=incident_direct_par_irradiance,
+        incident_diffuse_irradiance=incident_diffuse_par_irradiance,
+        solar_inclination=radians(solar_inclination_angle))
+    common_params = dict(
+        leaf_reflectance=0.08,
+        leaf_transmittance=0.07,
+        leaves_to_sun_average_projection=0.5,
+        sky_sectors_number=3,
+        sky_type='soc',
+        canopy_reflectance_to_diffuse_irradiance=0.057)
+    if is_lumped:
+        sim_inputs = irradiance_inputs.LumpedInputs(model='de_pury', **common_inputs)
+        sim_params = irradiance_params.LumpedParams(model='de_pury', **common_params)
     else:
-        sim_inputs = irradiance_inputs.SunlitShadedInputs(
-            leaf_layers=vegetative_layers,
-            incident_direct_irradiance=incident_direct_par_irradiance,
-            incident_diffuse_irradiance=incident_diffuse_par_irradiance,
-            solar_inclination=radians(solar_inclination_angle))
-        sim_params = irradiance_params.SunlitShadedParams(
-            leaf_reflectance=0.07,
-            leaf_transmittance=0.00,
-            leaves_to_sun_average_projection=0.5,
-            sky_sectors_number=3,
-            sky_type='soc',
-            canopy_reflectance_to_diffuse_irradiance=0.057)
-        sim_params.update(sim_inputs)
+        sim_inputs = irradiance_inputs.SunlitShadedInputs(**common_inputs)
+        sim_params = irradiance_params.SunlitShadedParams(**common_params)
+    sim_params.update(sim_inputs)
 
     return sim_inputs, sim_params
 
 
 def calc_absorbed_irradiance(
-        leaf_scale_type: str,
-        leaf_class_type: str,
+        is_bigleaf: bool,
+        is_lumped: bool,
         incident_direct_par_irradiance: float,
         incident_diffuse_par_irradiance: float,
         solar_inclination_angle: float) -> dict:
     inputs, params = get_irradiance_sim_inputs_and_params(**locals())
+    leaves_category = 'lumped' if is_lumped else 'sunlit-shaded'
     canopy = irradiance_canopy.Shoot(
-        leaves_category=leaf_class_type,
+        leaves_category=leaves_category,
         inputs=inputs,
         params=params)
+    canopy.calc_absorbed_irradiance()
 
     absorbed_par_irradiance = {index: layer.absorbed_irradiance for index, layer in canopy.items()}
 
@@ -109,15 +108,15 @@ def get_energy_balance_inputs_and_params(
          "absorbed_photosynthetically_active_radiation": absorbed_par_irradiance})
 
     if leaf_class_type == 'lumped':
-        inputs = eb_inputs.LumpedInputs(raw_inputs)
-        params = eb_params.LumpedParams(json_params)
+        energy_balance_inputs = eb_inputs.LumpedInputs(raw_inputs)
+        energy_balance_params = eb_params.LumpedParams(json_params)
     else:
-        inputs = eb_inputs.SunlitShadedInputs(raw_inputs)
-        params = eb_params.SunlitShadedParams(json_params)
+        energy_balance_inputs = eb_inputs.SunlitShadedInputs(raw_inputs)
+        energy_balance_params = eb_params.SunlitShadedParams(json_params)
 
-    params.update(inputs=inputs)
+    energy_balance_params.update(inputs=energy_balance_inputs)
 
-    return inputs, params
+    return energy_balance_inputs, energy_balance_params
 
 
 def calc_temperature(
@@ -145,24 +144,6 @@ def calc_temperature(
 #     value = {'lumped': []} if leaves_category == 'lumped' else {'sunlit': [], 'shaded': []}
 #
 #     return {k: value for k in canopy_layers.keys()}
-
-
-def get_weather_data(file_name: str) -> pd.DataFrame:
-    raw_data = pd.read_csv(file_name, decimal='.', sep=';', skiprows=6).set_index('time')
-    raw_data.loc[:, 'wind_speed'] = raw_data.apply(lambda x: x['wind_speed'] * 3600.0, axis=1)
-    raw_data.loc[:, 'incident_direct_irradiance'] = raw_data['incident_global_irradiance'].apply(
-        lambda x: utils.convert_global_irradiance_into_photosynthetically_active_radiation(x * 0.80))
-    raw_data.loc[:, 'incident_diffuse_irradiance'] = raw_data['incident_global_irradiance'].apply(
-        lambda x: utils.convert_global_irradiance_into_photosynthetically_active_radiation(x * 0.20))
-    raw_data.loc[:, 'vapor_pressure_deficit'] = raw_data.apply(
-        lambda x: utils.calc_vapor_pressure_deficit(x['air_temperature'], x['air_temperature'], x['relative_humidity']),
-        axis=1)
-    raw_data.loc[:, 'vapor_pressure'] = raw_data.apply(
-        lambda x: x['vapor_pressure_deficit'] * x['relative_humidity'] / 100., axis=1)
-
-    raw_data.drop(['incident_global_irradiance', 'relative_humidity'], axis=1, inplace=True)
-
-    return raw_data
 
 
 def plot_irradiance_dynamic_comparison(incident_irradiance: pd.Series,
@@ -296,24 +277,34 @@ def plot_temperature_dynamics(ax: plt.axis,
 
 
 def plot_temperature_one_hour_comparison(hour: int,
-                                         temperature_air: pd.Series,
+                                         hourly_weather: pd.DataFrame,
+                                         all_cases_absorbed_irradiance: dict,
                                          all_cases_temperature: dict):
+    assert all_cases_temperature.keys() == all_cases_absorbed_irradiance.keys()
+
     cases = all_cases_temperature.keys()
 
-    fig, axes = plt.subplots(ncols=len(cases), sharex='all', sharey='all', figsize=(15, 5))
+    fig, axes = plt.subplots(nrows=2, ncols=len(cases), sharex='row', sharey='all', figsize=(15, 10))
     for i, case in enumerate(cases):
         plot_temperature_at_one_hour(
-            ax=axes[i],
+            ax=axes[1, i],
             hour=hour,
-            temperature_air=temperature_air,
+            temperature_air=hourly_weather.loc[:, 'air_temperature'],
+            simulation_case=case,
+            all_cases_data=all_cases_temperature)
+        plot_irradiance_at_one_hour(
+            ax=axes[0, i],
+            hour=hour,
+            incident_direct=hourly_weather.loc[:, 'incident_direct_irradiance'],
+            incident_diffuse=hourly_weather.loc[:, 'incident_diffuse_irradiance'],
             simulation_case=case,
             all_cases_data=all_cases_temperature)
 
-    for i, ax in enumerate(axes):
+    for i, ax in enumerate(axes.flatten()):
         ax.legend()
-        ax.set(xlabel='hour')
-        if i == 0:
-            ax.set_ylabel(r'$\mathregular{temperature\/[^\circ C]}$')
+
+    for ax in axes[:, 0]:
+        ax.set_ylabel('Component index [-]')
 
     fig.tight_layout()
     fig.savefig('temperature_at_one_hour.png')
@@ -329,7 +320,6 @@ def plot_temperature_at_one_hour(ax: plt.axis,
     _, leaf_class = simulation_case.split('_')
     component_indexes = summary_data.keys()
 
-    ax.set_title(simulation_case.replace('_', ' '))
     ax.axvline(temperature_air[hour], label='air', color='k', linestyle='--', linewidth=2)
 
     if leaf_class == 'lumped':
@@ -343,33 +333,41 @@ def plot_temperature_at_one_hour(ax: plt.axis,
         ax.plot([(v - 273.15 if v > 273.15 else None) for v in x_sh], y, 'o-', label='shaded')
         ax.plot([(v - 273.15 if v > 273.15 else None) for v in x_sun], y, 'o-', label='sunlit')
 
-    pass
+    ax.set_xlabel('hour')
+    return
 
 
-def plot_weather(actual_wather: pd.DataFrame):
-    fig, ((ax_irradiance, ax_temperature), (ax_wind_speed, ax_vpd)) = plt.subplots(ncols=2, nrows=2, sharex='all')
+def plot_irradiance_at_one_hour(ax: plt.axis,
+                                hour: int,
+                                incident_direct: pd.Series,
+                                incident_diffuse: pd.Series,
+                                simulation_case: str,
+                                all_cases_data: dict):
+    summary_data = get_summary_data(simulation_case, all_cases_data)
+    _, leaf_class = simulation_case.split('_')
+    component_indexes = summary_data.keys()
 
-    day_hours = range(24)
-    ax_irradiance.plot(
-        day_hours, actual_wather.loc[:, ['incident_direct_irradiance', 'incident_diffuse_irradiance']].sum(axis=1))
-    ax_irradiance.set_ylabel(r'$\mathregular{irradiance\/[W_{PAR} \cdot m^{-2}_{ground}]}$')
+    ax.set_title(simulation_case.replace('_', ' '))
+    ax.axvline(incident_direct[hour], label='incident direct', color='y', linestyle='--', linewidth=2)
+    ax.axvline(incident_diffuse[hour], label='incident diffuse', color='red', linestyle='--', linewidth=2)
 
-    ax_temperature.plot(day_hours, actual_wather.loc[:, 'air_temperature'])
-    ax_temperature.set_ylabel(r'$\mathregular{temperature\/[^\circ C]}$')
+    if leaf_class == 'lumped':
+        y, x = zip(*[(i, summary_data[i][hour]) for i in component_indexes if i != -1])
+        ax.plot(x, y, 'o-', label='lumped')
+    else:
+        y, x_sun, x_sh = zip(
+            *[(i, summary_data[i]['sunlit'][hour], summary_data[i]['shaded'][hour]) for i in component_indexes if
+              i != -1])
 
-    ax_wind_speed.plot(day_hours, actual_wather.loc[:, 'wind_speed'] / 3600.)
-    ax_wind_speed.set_ylabel(r'$\mathregular{wind\/speed\/[m \cdot s^{-1}]}$')
+        ax.plot(x_sun, y, 'o-', label='sunlit')
+        ax.plot(x_sh, y, 'o-', label='shaded')
 
-    ax_vpd.plot(day_hours, actual_wather.loc[:, 'vapor_pressure_deficit'])
-    ax_vpd.set_ylabel('VPD [kPa]')
-
-    fig.tight_layout()
-    fig.savefig('weather.png')
-    plt.close()
+    ax.set_xlabel(r'$\mathregular{[W \cdot m^{-2}_{ground}]}$')
+    return
 
 
 if __name__ == '__main__':
-    weather_data = get_weather_data('weather.csv')
+    weather_data = get_weather_data()
 
     irradiance = {}
     temperature = {}
@@ -393,8 +391,8 @@ if __name__ == '__main__':
             air_temperature = w_data['air_temperature']
 
             absorbed_irradiance = calc_absorbed_irradiance(
-                leaf_scale_type=canopy_type,
-                leaf_class_type=leaves_type,
+                is_bigleaf=(canopy_type == 'bigleaf'),
+                is_lumped=(leaves_type == 'lumped'),
                 incident_direct_par_irradiance=incident_direct_irradiance,
                 incident_diffuse_par_irradiance=incident_diffuse_irradiance,
                 solar_inclination_angle=solar_inclination)
@@ -410,8 +408,6 @@ if __name__ == '__main__':
         irradiance[f'{canopy_type}_{leaves_type}'] = hourly_absorbed_irradiance
         temperature[f'{canopy_type}_{leaves_type}'] = hourly_temperature
 
-    plot_weather(weather_data)
-
     plot_irradiance_dynamic_comparison(
         incident_irradiance=(weather_data.loc[:, ['incident_direct_irradiance', 'incident_diffuse_irradiance']]).sum(
             axis=1),
@@ -423,5 +419,6 @@ if __name__ == '__main__':
 
     plot_temperature_one_hour_comparison(
         hour=12,
-        temperature_air=weather_data.loc[:, 'air_temperature'],
+        hourly_weather=weather_data,
+        all_cases_absorbed_irradiance=irradiance,
         all_cases_temperature=temperature)
